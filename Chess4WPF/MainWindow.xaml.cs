abd2b2015b7040aa648f6bda660a7e7f3fc70010
Марсель Баринов <barinov.marsel1111@gmail.com>
2021-05-31 13:11:26 +0300

    Исправлена ошибка с библиотекой классов

--- Chess4WPF/MainWindow.xaml.cs
+++ Chess4WPF/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Chess4WPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
--- Library/Class1.cs
+++ Library/Class1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Chess3
+namespace Library
 {
-    class Figure
+    public class Figure
     {
         public int x, y;
 
@@ -22,7 +22,7 @@ namespace Chess3
         }
     }
 
-    class King : Figure
+    public class King : Figure
     {
         public King(int newX, int newY) : base(newX, newY)
         {
@@ -36,7 +36,7 @@ namespace Chess3
         }
     }
 
-    class Queen : Figure
+     public class Queen : Figure
     {
         public Queen(int newX, int newY) : base(newX, newY)
         {
@@ -50,7 +50,7 @@ namespace Chess3
         }
     }
 
-    class Bishop : Figure
+    public class Bishop : Figure
     {
         public Bishop(int newX, int newY) : base(newX, newY)
         {
@@ -64,7 +64,7 @@ namespace Chess3
         }
     }
 
-    class Knight : Figure
+    public class Knight : Figure
     {
         public Knight(int newX, int newY) : base(newX, newY)
         {
@@ -78,7 +78,7 @@ namespace Chess3
         }
     }
 
-    class Rook : Figure
+    public class Rook : Figure
     {
         public Rook(int newX, int newY) : base(newX, newY)
         {
@@ -92,7 +92,7 @@ namespace Chess3
         }
     }
 
-    class Pawn : Figure
+    public class Pawn : Figure
     {
         public Pawn(int newX, int newY) : base(newX, newY)
         {
@@ -105,3 +105,4 @@ namespace Chess3
             return ((x == newX && y == 2 && y + 2 >= newY) || (x == newX && y + 1 == newY));
         }
     }
+}
